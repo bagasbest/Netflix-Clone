d303f142b332e7458e4d99577ca5e758b48b2ca9
Bagas Pangestu <bagas.p@elabram.com>
2025-06-30 21:41:10 +0700

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Bagas Pangestu on 30/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

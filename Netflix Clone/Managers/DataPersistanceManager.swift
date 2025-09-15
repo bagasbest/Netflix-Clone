816a5a3f1d293f6e2f31b453df01701d1792a5f1
Bagas Pangestu <bagas.p@elabram.com>
2025-09-15 08:32:04 +0700

    init core data swift

---
+++ Netflix Clone/Managers/DataPersistanceManager.swift
@@ -0,0 +1,16 @@
+//
+//  DataPersistanceManager.swift
+//  Netflix Clone
+//
+//  Created by Bagas Pangestu on 15/09/25.
+//
+
+import Foundation
+
+class DataPersistanceManager {
+    static let shared = DataPersistanceManager()
+    
+    func downloadTitleWith(model: Title, completion: @escaping (Result<Void, Error>) -> Void) {
+        
+    }
+}
